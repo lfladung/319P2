22cf1b776f762e6d6f5f8f32daf71719c3d568ed
lfladung <lfladung@iastate.edu>
2017-12-09 22:33:11 -0600

    Fixed

--- Portfolio2/src/app/login/login.component.ts
+++ Portfolio2/src/app/login/login.component.ts
@@ -9,13 +9,13 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   selector: 'app-login',
   templateUrl: './login.component.html'
 })
-export class LoginComponent 
-{   
+export class LoginComponent
+{
     @ViewChild('username') username: ElementRef;
     @ViewChild('password') password: ElementRef;
 
     registerForm: FormGroup;
-    
+
     constructor(private UserService: UserService, private router: Router, private fb: FormBuilder) {
         this.registerForm = this.fb.group({
             username: [null, Validators.required],
@@ -30,7 +30,8 @@ export class LoginComponent
     HandleLoginSubmit() {
         this.UserService.login(this.username.nativeElement.value, this.password.nativeElement.value).subscribe(res => {
             localStorage.setItem("User", JSON.stringify(res));
-            this.router.navigate(['shop', 1]);
+            var info = JSON.parse(localStorage.getItem("User"));
+            this.router.navigate(['shop', info.Prefered]);
         }, err => {
             console.log(err);
         } );
@@ -40,7 +41,7 @@ export class LoginComponent
         let value = this.registerForm.value;
         this.UserService.register(value.username, value.password, value.gender, value.prefered, value.birthday, value.cash).subscribe(res => {
             localStorage.setItem("User", JSON.stringify(res));
-            this.router.navigate(['shop', 1]);
+            this.router.navigate(['shop', value.prefered]);
         }, err => {
             console.log(err);
         })
