97fbf1dc1504d4043d941ddd47125c7426dc33ce
AustinKeen <akeen@iastate.edu>
2017-12-10 14:34:20 -0600

    updated

--- Portfolio2/src/app/shop/shop.component.ts
+++ Portfolio2/src/app/shop/shop.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ItemService } from '../services/items.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-shop',
@@ -7,6 +9,44 @@ import { Component } from '@angular/core';
 
 export class ShopComponent 
 {
-    constructor() {
-    }
+  items: any=[];
+  Cart: any={ID:1};
+  finItems: any=[];
+
+  constructor(private ItemService: ItemService, private router: Router) {
+    this.ItemService.getAll().subscribe(res => {
+      var info = JSON.parse(JSON.stringify(res));
+      for(var i = 0; i < info.length; i++)
+      {
+        for(var j = 1; j < 5; j++)
+        {
+          if(this.router.url == "/shop/" + j && info[i].category == j)
+          {
+            console.log(info[i]);
+            this.finItems.push(info[i]);
+          }
+        }
+      }
+      this.items=this.finItems;
+    })
+  }
+
+  handlePageChange()
+  {
+    this.ItemService.getAll().subscribe(res => {
+      var info = JSON.parse(JSON.stringify(res));
+      for(var i = 0; i < info.length; i++)
+      {
+        for(var j = 1; j < 5; j++)
+        {
+          if(this.router.url == "/shop/" + j && info[i].category == j)
+          {
+            console.log(info[i]);
+            this.finItems.push(info[i]);
+          }
+        }
+      }
+      this.items=this.finItems;
+    })
+  }
 }
